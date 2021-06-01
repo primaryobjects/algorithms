42384c8f1951fd56c76c9094692054cc1920a116
Kory Becker <kbecker@primaryobjects.com>
2021-06-01 15:15:25 -0400

    Added comments.

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -7,32 +7,121 @@ public final class Solver {
     private final Stack<Board> solutionBoards;
     private boolean isSolvable;
 
+    public Solver(Board initial) 
+    {
+        if (initial == null) 
+        {
+            throw new IllegalArgumentException();
+        }
 
-    public Solver(Board initial) {
-        if (initial == null) throw new IllegalArgumentException();
+        // Initialize our starting state.
         isSolvable = false;
         solutionBoards = new Stack<>();
+
+        // Use a minimum priority queue to store potential board moves.
         MinPQ<SearchNode> searchNodes = new MinPQ<>();
 
+        // Insert the starting board position and a second board with one tile swapped.
         searchNodes.insert(new SearchNode(initial, null));
         searchNodes.insert(new SearchNode(initial.twin(), null));
 
-        while (!searchNodes.min().board.isGoal()) {
+        while (!searchNodes.min().board.isGoal())
+        {
+            // Get the next least-cost board move.
             SearchNode searchNode = searchNodes.delMin();
+
+            // Find all possible next moves.
             for (Board board : searchNode.board.neighbors())
-                if (searchNode.prevNode == null || searchNode.prevNode != null && !searchNode.prevNode.board.equals(board))
+            {
+                // Avoid duplicate board.
+                if (searchNode.prevNode == null || (searchNode.prevNode != null && !searchNode.prevNode.board.equals(board)))
+                {
+                    // We have a new board move, insert it into the priority queue.
                     searchNodes.insert(new SearchNode(board, searchNode));
+                }
+            }
         }
 
+        // We've arrived at a goal from either our initial starting board or from its twin.
+        // Find the goal state, which will be the lowest cost node.
         SearchNode current = searchNodes.min();
-        while (current.prevNode != null) {
+
+        // Walk backwards from the goal state to the initial board.
+        while (current.prevNode != null)
+        {
             solutionBoards.push(current.board);
             current = current.prevNode;
         }
+
+        // Finally, add the last board (our starting board).
         solutionBoards.push(current.board);
 
-        if (current.board.equals(initial)) isSolvable = true;
+        // If we've arrived back at our starting board, this game is solvable. Otherwise, it's the twin.
+        if (current.board.equals(initial))
+        {
+            isSolvable = true;
+        }
+    }
 
+    private class SearchNode implements Comparable<SearchNode> {
+        private final Board board;
+        private final SearchNode prevNode;
+        private int moves;
+        private int manhattan;
+
+        public SearchNode(Board board, SearchNode prevNode) {
+            this.board = board;
+            this.prevNode = prevNode;
+
+            // Calculate the cost using the manhattan distance.
+            this.manhattan = board.manhattan();
+
+            if (prevNode != null)
+            {
+                // Bump the penalty cost +1 for each move from the beginning.
+                moves = prevNode.moves + 1;
+            }
+            else
+            {
+                // Initialize cost penalty to 0.
+                moves = 0;
+            }
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            // A* = Estimated cost to goal (manhattan) plus the cost from start to current state (this.moves).
+            int priorityDiff = (this.manhattan + this.moves) - (that.manhattan + that.moves);
+
+            // If the two boards are equal cost, break the tie by ignoring the cost so far and just using the pure manhattan distance.
+            return  priorityDiff == 0 ? this.manhattan - that.manhattan : priorityDiff;
+        }
+    }
+
+    public int moves() {
+        int result = -1;
+
+        if (isSolvable())
+        {
+            result = solutionBoards.size() - 1;
+        }
+
+        return result;
+    }
+
+    public Iterable<Board> solution() {
+        Iterable<Board> result = null;
+
+        if (isSolvable())
+        {
+            result = solutionBoards;
+        }
+
+        return result;
+    }
+
+    public boolean isSolvable() {
+        return isSolvable;
     }
 
     public static void main(String[] args) {
@@ -60,41 +149,6 @@ public final class Solver {
             StdOut.println("Minimum number of moves = " + solver.moves());
             for (Board board : solver.solution())
                 StdOut.println(board);
-        }
-    }
-
-    public int moves() {
-        if (!isSolvable()) return -1;
-        return solutionBoards.size() - 1;
-    }
-
-    public Iterable<Board> solution() {
-        if (isSolvable()) return solutionBoards;
-        return null;
-    }
-
-    public boolean isSolvable() {
-        return isSolvable;
-    }
-
-    private class SearchNode implements Comparable<SearchNode> {
-        private final Board board;
-        private final SearchNode prevNode;
-        private int moves;
-        private int manhattan;
-
-        public SearchNode(Board board, SearchNode prevNode) {
-            this.board = board;
-            this.prevNode = prevNode;
-            this.manhattan = board.manhattan();
-            if (prevNode != null) moves = prevNode.moves + 1;
-            else moves = 0;
-        }
-
-        @Override
-        public int compareTo(SearchNode that) {
-            int priorityDiff = (this.manhattan + this.moves) - (that.manhattan + that.moves);
-            return  priorityDiff == 0 ? this.manhattan - that.manhattan : priorityDiff;
         }
     }
 }
